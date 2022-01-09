4ab8471cd73fa9e2217553540bb8c623c5e6696d
Alessandro Toschi <aletos@plexagon.it>
2022-01-09 14:59:16 +0100

    Initial Commit

---
+++ CameraPreview/CameraPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPreviewApp.swift
+//  CameraPreview
+//
+//  Created by Alessandro Toschi on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
